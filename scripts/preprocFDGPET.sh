9c2006c7fba0abf6d4905c010ef4f225cc8da4d7
ltuominen <tuominenlj@gmail.com>
2023-06-19 10:47:36 -0400

    reorganize preproc

--- scripts/preprocFDGPET.sh
+++ scripts/preprocFDGPET.sh
@@ -2,86 +2,145 @@
 
 ## preproc FDG PET data
 
-input=$1 # path to raw pet scan (pet.nii)
-subject=$2 # subject name as in SUBJECTS_DIR
-derivates=$3 # path to derivates folder
-iter=$4 # name of iter folder in the derivates folder
+make_mask() {
+  target=$1
 
-pet=$( basename $input )
-spl_pet=(${pet//_/ })
-pn=(${spl_pet[0]//-/ })
+  targ_dir=$(dirname $target)
+  temp=${targ_dir}/temp
+  mkdir ${temp}
 
-# create folders if missing
-if [ ! -d  ${derivates}/${iter} ]; then mkdir ${derivates}/${iter}; fi
-if [ ! -d ${derivates}/${iter}/sub-${pn[1]} ]; then mkdir ${derivates}/${iter}/sub-${pn[1]}; fi
-if [ ! -d ${derivates}/${iter}/sub-${pn[1]}/ses-${pn[3]} ]; then mkdir ${derivates}/${iter}/sub-${pn[1]}/ses-${pn[3]}; fi
+  mri_convert $target ${temp}/aparc+aseg.nii.gz
 
-pdir=${derivates}/${iter}/sub-${pn[1]}/ses-${pn[3]}/pet
+  fslmaths ${temp}/aparc+aseg.nii.gz -thr 24 -uthr 24 -bin ${temp}/CSF_mask.nii.gz
 
-# create PET directory in the derivates and make a copy of the raw pet.nii 
-mkdir $pdir
-cp $input $pdir
+  add=(4 5 14 15 43 44 72 73)
+  for r in ${add[@]};do
+    fslmaths $target -thr $r -uthr $r -bin ${temp}/${r}.nii.gz
+    fslmaths ${temp}/CSF_mask.nii.gz -add ${temp}/${r}.nii.gz -bin ${temp}/CSF_mask.nii.gz
+  done
 
-echo "----------------------------------------------------------------------------------------------------------------
-Doing motion correction for ${pdir}/${pet}.
-----------------------------------------------------------------------------------------------------------------"
+  fslmaths ${temp}/aparc+aseg.nii.gz -bin ${temp}/aparc+aseg_bin.nii.gz
+  fslmaths ${temp}/aparc+aseg_bin.nii.gz -sub ${temp}/CSF_mask.nii.gz ${temp}/wmgm_mask.nii.gz
 
-# motion correct
-mcflirt -in ${pdir}/${pet} -refvol 0 -out $pdir/mc_${pet} # out = mc_p1.nii
+  cp ${temp}/CSF_mask.nii.gz $targ_dir
+  cp ${temp}/wmgm_mask.nii.gz $targ_dir
 
-echo "----------------------------------------------------------------------------------------------------------------
-Summing up frames of $pdir/mc_${pet}.
-----------------------------------------------------------------------------------------------------------------"
+  rm -r ${temp}
 
-# sum
-mri_concat $pdir/mc_${pet}.gz --sum --o $pdir/sum_mc_${pet}.gz # out = sum_mc_p1.nii.gz
+}
 
-echo "----------------------------------------------------------------------------------------------------------------
-Reslicing $pdir/sum_mc_${pet}.gz.
-----------------------------------------------------------------------------------------------------------------"
+preproc() {
+  input=$1 # path to raw pet scan (pet.nii)
+  subject=$2 # subject name as in SUBJECTS_DIR
+  iter=$3 # path to iter folder in the derivates folder
 
-# reslice
-mri_convert $pdir/sum_mc_${pet}.gz -vs 2 2 2 $pdir/rs_sum_mc_${pet}.gz --force_ras_good # out = rs_sum_mc_p1.nii.gz
+  # get session name etc
+  pet=$( basename $input )
+  spl_pet=(${pet//_/ })
+  pn=(${spl_pet[0]//-/ })
 
-echo "----------------------------------------------------------------------------------------------------------------
-Coregistering $pdir/rs_sum_mc_${pet}.gz to ${subject}.
-----------------------------------------------------------------------------------------------------------------"
+  # create folders if missing
+  if [ ! -d  ${derivates}/${iter} ]; then mkdir ${derivates}/${iter}; fi
+  if [ ! -d ${derivates}/${iter}/sub-${pn[1]} ]; then mkdir ${derivates}/${iter}/sub-${pn[1]}; fi
+  if [ ! -d ${derivates}/${iter}/sub-${pn[1]}/ses-${pn[3]} ]; then mkdir ${derivates}/${iter}/sub-${pn[1]}/ses-${pn[3]}; fi
 
-# coregister to T1
-mri_coreg --s ${subject} --targ $SUBJECTS_DIR/${subject}/mri/brain.mgz --no-ref-mask --mov $pdir/rs_sum_mc_${pet}.gz --reg $pdir/p2mri1.reg.lta --dof 9 --threads 3 # out = p2mri1.reg.lta
+  pdir=${derivates}/${iter}/sub-${pn[1]}/ses-${pn[3]}/pet
 
-# move to T1
-mri_vol2vol --reg $pdir/p2mri1.reg.lta --mov $pdir/rs_sum_mc_${pet}.gz --fstarg --o $pdir/in-anat-${pet}.gz  # out = in-anat-p.nii.gz
+  # create PET directory in the derivates and make a copy of the raw pet.nii
+  mkdir $pdir
+  cp $input $pdir
 
-echo "----------------------------------------------------------------------------------------------------------------
-Calculating SUVR from $pdir/rs_sum_mc_${pet}.gz.
-----------------------------------------------------------------------------------------------------------------"
+  echo "----------------------------------------------------------------------------------------------------------------
+  Doing motion correction for ${pdir}/${pet}.
+  ----------------------------------------------------------------------------------------------------------------"
 
-# make whitematter + graymatter mask
-./make_mask.sh ${SUBJECTS_DIR}/${subject}/mri/aparc+aseg.mgz
-mask=${SUBJECTS_DIR}/${subject}/mri/wmgm_mask.nii.gz
+  # motion correct
+  mcflirt -in ${pdir}/${pet} -refvol 0 -out $pdir/mc_${pet} # out = mc_p1.nii
 
-# extract average value within the mask from the image
-fslstats $pdir/in-anat-${pet}.gz -k $mask -M > ${pdir}/wmgm_mean_activity
-R=$( cat ${pdir}/wmgm_mean_activity )
+  echo "----------------------------------------------------------------------------------------------------------------
+  Summing up frames of $pdir/mc_${pet}.
+  ----------------------------------------------------------------------------------------------------------------"
 
-# dived the image by the average value
-fslmaths $pdir/rs_sum_mc_${pet}.gz -div $R ${pdir}/SUVR.nii.gz
-fslmaths $pdir/in-anat-${pet}.gz -div $R ${pdir}/SUVR.in.anat.nii.gz
+  # sum
+  mri_concat $pdir/mc_${pet}.gz --sum --o $pdir/sum_mc_${pet}.gz # out = sum_mc_p1.nii.gz
 
-echo "----------------------------------------------------------------------------------------------------------------
-Registering ${subject} to MNI152
-----------------------------------------------------------------------------------------------------------------"
+  echo "----------------------------------------------------------------------------------------------------------------
+  Reslicing $pdir/sum_mc_${pet}.gz.
+  ----------------------------------------------------------------------------------------------------------------"
 
-# create a map to MNI152
-mni152reg --s ${subject}
+  # reslice
+  mri_convert $pdir/sum_mc_${pet}.gz -vs 2 2 2 $pdir/rs_sum_mc_${pet}.gz --force_ras_good # out = rs_sum_mc_p1.nii.gz
 
-echo "----------------------------------------------------------------------------------------------------------------
-Normalizing and smoothing ${pdir}/SUVR.nii.gz.
-----------------------------------------------------------------------------------------------------------------"
+  echo "----------------------------------------------------------------------------------------------------------------
+  Coregistering $pdir/rs_sum_mc_${pet}.gz to ${subject}.
+  ----------------------------------------------------------------------------------------------------------------"
 
-# move SUVR to MNI152
-mri_vol2vol --mov $pdir/SUVR.nii.gz --reg $pdir/p2mri1.reg.lta --mni152reg --talres 2 --o $pdir/SUVR.mni152.2mm.sm00.nii.gz #out = SUVR1.mni152.2mm.sm00.nii.gz
+  # coregister to T1
+  mri_coreg --s ${subject} --targ $SUBJECTS_DIR/${subject}/mri/brain.mgz --no-ref-mask --mov $pdir/rs_sum_mc_${pet}.gz --reg $pdir/p2mri1.reg.lta --dof 9 --threads 3 # out = p2mri1.reg.lta
 
-# smooth SUVRs
-fslmaths $pdir/SUVR.mni152.2mm.sm00.nii.gz -s 8 $pdir/SUVR.mni152.2mm.sm08.nii.gz # out = SUVR1.mni152.2mm.sm05.nii.gz
+  # move to T1
+  mri_vol2vol --reg $pdir/p2mri1.reg.lta --mov $pdir/rs_sum_mc_${pet}.gz --fstarg --o $pdir/in-anat-${pet}.gz  # out = in-anat-p.nii.gz
+
+  echo "----------------------------------------------------------------------------------------------------------------
+  Calculating SUVR from $pdir/rs_sum_mc_${pet}.gz.
+  ----------------------------------------------------------------------------------------------------------------"
+
+  # make whitematter + graymatter mask
+  make_mask ${SUBJECTS_DIR}/${subject}/mri/aparc+aseg.mgz
+  mask=${SUBJECTS_DIR}/${subject}/mri/wmgm_mask.nii.gz
+
+  # extract average value within the mask from the image
+  fslstats $pdir/in-anat-${pet}.gz -k $mask -M > ${pdir}/wmgm_mean_activity
+  R=$( cat ${pdir}/wmgm_mean_activity )
+
+  # capture ref values
+  echo ${subject} ses-${pn[3]} $R >> wmgm_mean_activity_values.txt
+
+  # dived the image by the average value
+  fslmaths $pdir/rs_sum_mc_${pet}.gz -div $R ${pdir}/SUVR.nii.gz
+  fslmaths $pdir/in-anat-${pet}.gz -div $R ${pdir}/SUVR.in.anat.nii.gz
+
+  echo "----------------------------------------------------------------------------------------------------------------
+  Registering ${subject} to MNI152
+  ----------------------------------------------------------------------------------------------------------------"
+
+  # create a map to MNI152
+  mni152reg --s ${subject}
+
+  echo "----------------------------------------------------------------------------------------------------------------
+  Normalizing and smoothing ${pdir}/SUVR.nii.gz.
+  ----------------------------------------------------------------------------------------------------------------"
+
+  # move SUVR to MNI152
+  mri_vol2vol --mov $pdir/SUVR.nii.gz --reg $pdir/p2mri1.reg.lta --mni152reg --talres 2 --o $pdir/SUVR.mni152.2mm.sm00.nii.gz #out = SUVR1.mni152.2mm.sm00.nii.gz
+
+  # smooth SUVRs
+  fslmaths $pdir/SUVR.mni152.2mm.sm00.nii.gz -s 8 $pdir/SUVR.mni152.2mm.sm08.nii.gz # out = SUVR1.mni152.2mm.sm05.nii.gz
+
+  # catch error status
+  echo ${subject} ses-${pn[3]} $? >> error.log
+
+}
+
+run_all() {
+  list_of_subjects=$1
+
+  while read line; do
+    array=( $line );
+    subject=${array[0]}
+    pet002=/group/tuominen/TBS-FDG/rawdata/sub-${subject}/ses-002/pet/sub-${subject}-ses-002_pet.nii.gz
+    pet003=/group/tuominen/TBS-FDG/rawdata/sub-${subject}/ses-003/pet/sub-${subject}-ses-003_pet.nii.gz
+    mri002=sub-${subject}_ses-002
+    mri003=sub-${subject}_ses-003
+    derivates=/group/tuominen/TBS-FDG/derivatives
+    iter=iter5
+
+    preproc $pet002 $mri002 $derivates $iter5 &
+    preproc $pet003 $mri003 $derivates $iter5
+
+  done < $list_of_subjects
+
+}
+
+
+run_all $1
