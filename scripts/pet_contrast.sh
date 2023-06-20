9ef04c9439fe65d60ef54e0acfb64497b1125a34
ltuominen <tuominenlj@gmail.com>
2023-06-20 12:22:34 -0400

    contrast go2

--- scripts/pet_contrast.sh
+++ scripts/pet_contrast.sh
@@ -14,6 +14,6 @@ while read line; do
   sham_pet=${derivates}/sub-${subject}/$sham_ses/pet/SUVR.mni152.2mm.sm08.nii.gz
   active_pet=${derivates}/sub-${subject}/$active_ses/pet/SUVR.mni152.2mm.sm08.nii.gz
   fslmaths $active_pet -sub $sham_pet ${outputfolder}/${subject}_active_minus_sham_mni152_sm08.nii.gz
-  fslmaths ${outputfolder}/${subject}_active_minus_sham.nii.gz -div $sham_pet -mul 100 ${outputfolder}/${subject}_active_minus_sham_perc_diff.nii.gz
+  fslmaths ${outputfolder}/${subject}_active_minus_sham_mni152_sm08.nii.gz -div $sham_pet -mul 100 ${outputfolder}/${subject}_active_minus_sham_mni152_sm08_perc_diff.nii.gz
 
 done < <(tail -n +2 ../info/unmask.txt)
